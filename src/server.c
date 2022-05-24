e31dbc019383c08784e32720578282345430d2fb
Demjech <democrite3@gmail.com>
2022-05-24 18:37:29 +0200

    Fichiers server et client fonctionnel : Ils envoient le fichier client.txt et append le contenu au fichier server.txt Memento : ajouter un fichier server.txt vide au départ.

--- src/server.c
+++ src/server.c
@@ -6,8 +6,36 @@
 
 #define SIZE 1024
 
+void write_file(int sockfd, struct sockaddr_in addr) {
+  FILE *fp;
+  char *filename = "server.txt";
+  int n;
+  char buffer[SIZE];
+  socklen_t addr_size;
+
+  fp = fopen(filename, "at");
+
+  while(1) {
+    addr_size = sizeof(addr);
+    n = recvfrom(sockfd, buffer, SIZE, 0, (struct sockaddr*) &addr, &addr_size);
+
+    if (strcmp(buffer, "END") == 0) {
+      printf("%s\t%d", buffer, strcmp(buffer, "END"));
+      break;
+      return;
+    }
+
+    printf("[RECEIVING] Data: %s", buffer);
+    fprintf(fp, "%s", buffer);
+    bzero(buffer, SIZE);
+  }
+
+  fclose(fp);
+  return;
+}
+
 int main(int argc, char ** argv) {
-  char *ip = "127.0.0.1";
+  char *ip = "127.0.0.1";                       //Addr IP de rebouclage
   int port = 8080;
 
   int server_sockfd;
@@ -26,11 +54,19 @@ int main(int argc, char ** argv) {
   server_addr.sin_port = port;
   server_addr.sin_addr.s_addr = inet_addr(ip);
 
-  e = bind(server_sockfd, (struct sockaddr_in *)&server_addr, sizeof(server_addr));
+  e = bind(server_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
   if (e < 0) {
     printf("[ERROR] Bind error, exit code : %d\n", e);
     exit(e);
   }
 
-  printf("[STARTING] UDP File Server started.\n", );
+  printf("[STARTING] UDP File Server started.\n");
+  write_file(server_sockfd, client_addr);
+
+  printf("[SUCCESS] Data transfer complete.\n");
+  printf("[CLOSING] Closing the server.\n");
+
+  close(server_sockfd);
+
+  return 0;
 }
